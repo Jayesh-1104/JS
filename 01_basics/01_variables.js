decf3f8f9abc095847ff4f16d75198d0f79a5921
Jayesh-1104 <146550793+Jayesh-1104@users.noreply.github.com>
2025-12-01 09:37:56 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "jahyesh@gmail.com"
+var accountPassword  = "789456"
+accountcity = "nashik"
+
+//console.log(accountcity);
+accountEmail = "pandit@gmail.com"
+accountPassword = "654684"
+accountcity = "mumbai"
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountcity);
+/*use table also
+ */
