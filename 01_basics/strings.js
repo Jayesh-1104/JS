c59c5bfb1dfe9309f810917ae7c8ab3090b035c0
Jayesh-1104 <146550793+Jayesh-1104@users.noreply.github.com>
2026-01-02 18:51:17 +0000

    string oprn

---
+++ 01_basics/strings.js
@@ -0,0 +1,21 @@
+// const name = "Jayu"
+// console.log(name);
+
+// concatnation
+
+// const name = "Jayesh Shantilal Shewale"
+// const Rollno = 50 
+// const email = "shewalejayesh6@gmail.com"
+
+// console.log(`My name is ${name}. My emailand rollno is ${email} ${Rollno} respectively.`);
+const name = "Jayesh"
+console.log(name.length)
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.charAt(2));
+console.log(name.indexOf(2));
+
+
+
+
+
