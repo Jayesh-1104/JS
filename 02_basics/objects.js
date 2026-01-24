197c63f3733d7bd2b7622d5be761e8e83a301556
Jayesh-1104 <146550793+Jayesh-1104@users.noreply.github.com>
2026-01-24 11:44:48 +0000

    objects

--- 02_basics/objects.js
+++ 02_basics/objects.js
@@ -11,9 +11,28 @@ const Juser ={
     lastlogin: ["monday","saturday"]
 }
 
-console.log(Juser.email) //one method to access variables
-console.log(Juser["email"]);//another ethod to access variables
-console.log(Juser["time"]);
-console.log(Juser[mysysm]);//add key symbol 
-//asked in interview
+// console.log(Juser.email) //one method to access variables
+// console.log(Juser["email"]);//another ethod to access variables
+// console.log(Juser["time"]);
+// console.log(Juser[mysysm]);//add key symbol 
+// //asked in interview
+
+// Juser.email  = "jayesh@gmail.com"//change in values done by = 
+// console.log(Juser["email"]);
+
+// Object.freeze(Juser)//how to freeze them ,no changr further
+// Juser.email = " jayesh@jayu.com"
+// console.log(Juser);
+Juser.greeting = function(){
+    console.log("Hello");
+    }
+
+
+
+
+Juser.greetingtwo = function(){
+    console.log(`Hello ${this.name}`);//working of this
+    }
+console.log(Juser.greeting());
+console.log(Juser.greetingtwo());
 
