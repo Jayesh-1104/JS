9807061212355adc2909cd1bce6e1221a5d2b867
Jayesh-1104 <146550793+Jayesh-1104@users.noreply.github.com>
2026-01-24 17:28:58 +0000

    objects04

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -31,7 +31,24 @@ const regularuser = {
 const obj1 = {1:"a",2:"b"}
 const obj2 = {3:"a",4:"b"}
 const obj3 = Object.assign ({},obj1,obj2)
-console.log(obj3)
+//console.log(obj3)
+
+
+console.log(tinderuser);
+//{ id: '1', name: ' jayu', isLoggedIn: false }
+
+console.log(Object.keys(tinderuser))
+//[ 'id', 'name', 'isLoggedIn' ]
+
+console.log(Object.values(tinderuser))
+//[ '1', ' jayu', false ]
+
+console.log(Object.entries(tinderuser))
+//[ [ 'id', '1' ], [ 'name', ' jayu' ], [ 'isLoggedIn', false ] ]
+
+console.log(tinderuser.hasOwnProperty('isLoggedIn'));
+//true
+
 
 
 
